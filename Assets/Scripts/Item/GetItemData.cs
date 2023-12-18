23547a8369080c6b52ed20563dfb4206a3429c74
Sunkunwoo <aksiapice@naver.com>
2023-12-18 16:25:24 +0900

    주석추가
    주석추가

--- Assets/Scripts/Item/GetItemData.cs
+++ Assets/Scripts/Item/GetItemData.cs
@@ -9,21 +9,21 @@ public class GetItemData : MonoBehaviour, IInteractable
     void Start()
     {
         itemName = gameObject.name.Replace("(Clone)", "").Trim();
-        item = FindItem(itemName, ItemDataManager.Instance.resourceList);
+        item = FindItem(itemName, ItemDataManager.Instance.resourceList); // 해당 컴포넌트가 적용된 오브젝트에 해당 오브젝트와 동일한 이름을 사용하는 데이터를 resourceList에서 데이터를 찾아서 할당
 
         if (item == null)
         {
-            item = FindItem(itemName, ItemDataManager.Instance.foodList);
+            item = FindItem(itemName, ItemDataManager.Instance.foodList); // 해당 컴포넌트가 적용된 오브젝트에 해당 오브젝트와 동일한 이름을 사용하는 데이터를 foodList에서 데이터를 찾아서 할당
         }
 
         if (item == null)
         {
-            item = FindItem(itemName, ItemDataManager.Instance.medicinesList);
+            item = FindItem(itemName, ItemDataManager.Instance.medicinesList);// 해당 컴포넌트가 적용된 오브젝트에 해당 오브젝트와 동일한 이름을 사용하는 데이터를 medicinesList에서 데이터를 찾아서 할당
         }
 
         if (item == null)
         {
-            item = FindItem(itemName, ItemDataManager.Instance.equipmentItemItemList);
+            item = FindItem(itemName, ItemDataManager.Instance.equipmentItemItemList);// 해당 컴포넌트가 적용된 오브젝트에 해당 오브젝트와 동일한 이름을 사용하는 데이터를 equipmentItemItemList에서 데이터를 찾아서 할당
         }
         
     }
@@ -37,9 +37,9 @@ public class GetItemData : MonoBehaviour, IInteractable
     {
         return itemList.Find(item => item.name == itemName);
     }
-
     public string GetInteractPrompt()
     {
+        Inventory.instance.AddItem(item);
         return string.Format($"Pickup {item.name}");
     }
 

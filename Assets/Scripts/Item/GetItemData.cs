f1c3cb29c2bafbd1a1313d49d1de1ebc3aa93ee4
Sunkunwoo <aksiapice@naver.com>
2023-12-18 13:17:48 +0900

    GetItemData 코드 수정
    GetItemData 코드 수정

--- Assets/Scripts/Item/GetItemData.cs
+++ Assets/Scripts/Item/GetItemData.cs
@@ -5,16 +5,45 @@ public class GetItemData : MonoBehaviour
 {
     string itemName;
     Item item;
+    EquipmentItem equipmentItem;
 
     void Start()
     {
         itemName = gameObject.name.Replace("(Clone)", "").Trim();
         item = FindItem(itemName, ItemDataManager.Instance.resourceList);
-        Debug.Log($"Found item: {item.name}");
-        Debug.Log($"Found item: {item.index}");
+
+        if (item == null)
+        {
+            item = FindItem(itemName, ItemDataManager.Instance.foodList);
+        }
+
+        if (item == null)
+        {
+            item = FindItem(itemName, ItemDataManager.Instance.medicinesList);
+        }
+
+        equipmentItem = FindItem(itemName, ItemDataManager.Instance.equipmentItemItemList);
+
+        if (equipmentItem != null)
+        {
+            Debug.Log($"Found item: {equipmentItem.name}");
+            Debug.Log($"Found item: {equipmentItem.index}");
+        }
+
+        if (item != null)
+        {
+            Debug.Log($"Found item: {item.name}");
+            Debug.Log($"Found item: {item.index}");
+        }
+        
     }
 
     Item FindItem(string itemName, List<Item> itemList)
+    {
+        return itemList.Find(item => item.name == itemName);
+    }
+
+    EquipmentItem FindItem(string itemName, List<EquipmentItem> itemList)
     {
         return itemList.Find(item => item.name == itemName);
     }

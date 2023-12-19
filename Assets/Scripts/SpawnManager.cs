187ce7be188e2108cb341df0ca09fbcf4331f21b
ebanlee1204 <ebanlee1204@gmail.com>
2023-12-19 17:50:49 +0900

    Update Tree and Rock Spawner

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject Rock;
+    public GameObject Tree;
+    [SerializeField] public int treeCount;
+    [SerializeField] public int rockCount;
+    RaycastHit hit;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnRock();
+        SpawnTree();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnRock()
+    {
+        for (int i = 0; i < rockCount; i++)
+        {
+            Vector3 randomSpawnPosition = new Vector3(Random.Range(25f, 125f), 100, Random.Range(-35f, 75f));
+
+            if (Physics.Raycast(randomSpawnPosition, Vector3.down, out hit, 200, 8))
+            {
+                Instantiate(Rock, hit.point, Quaternion.identity);
+            }
+        }
+    }
+
+    void SpawnTree()
+    {
+        for (int i = 0; i < treeCount; i++)
+        {
+            Vector3 randomSpawnPosition = new Vector3(Random.Range(25f, 125f), 100, Random.Range(-35f, 75f));
+
+            if (Physics.Raycast(randomSpawnPosition, Vector3.down, out hit, 200, 8))
+            {
+                Instantiate(Tree, hit.point, Quaternion.identity);
+            }
+        }
+    }
+}

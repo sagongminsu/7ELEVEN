357b7806405ddd1940225ec2a4b816765526235b
Limseokchang <111727297+PsychoLim@users.noreply.github.com>
2023-12-19 13:33:42 +0900

    플레이어 공격 애니메이션 추가

--- Assets/Scripts/Player/EquipTool.cs
+++ Assets/Scripts/Player/EquipTool.cs
@@ -5,4 +5,38 @@ using UnityEngine;
 public class EquipTool : Equip
 {
     public float attackRate;
+    private bool attacking;
+    public float attackDistance;
+
+    [Header("Resource Gathering")]
+    public bool doesGatherResources;
+
+    [Header("Combat")]
+    public bool doesDealDamage;
+    public int damage;
+
+    private Animator animator;
+    private Camera camera;
+
+    private void Awake()
+    {
+        camera = Camera.main;
+        animator = GetComponent<Animator>();
+    }
+
+    public override void OnAttackInput()
+    {
+        if (!attacking)
+        {
+            attacking = true;
+            animator.SetTrigger("Attack");
+            Invoke("OnCanAttack", attackRate);
+        }
+    }
+
+    void OnCamAttack()
+    {
+        attacking = false;
+    }
+
 }

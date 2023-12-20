b3f8c073ae72cfe6d38bb4d13ace3e71d81c1f21
WooJin <106531904+h117562@users.noreply.github.com>
2023-12-20 14:27:32 +0900

    feat - 게임 오버 화면 구현

---
+++ Assets/Scripts/Button/GoMainButton.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoMainButton : MonoBehaviour
+{
+    public void OnClick()
+    {
+        Debug.Log("Go Main");
+    }
+}
